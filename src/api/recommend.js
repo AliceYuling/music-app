ee993e642388ecbc27f407f1896bbe8daf567b91
AliceYuling <yyl1215@126.com>
2017-10-17 20:38:04 +0800

    修改轮播图

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -1,5 +1,6 @@
 import jsonp from 'common/js/jsonp';
 import {commonParams, options} from './config';
+import axios from 'axios';
 
 export function getRecommend () {
   const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg';
@@ -28,5 +29,9 @@ export function getDiscList () {
     format: 'json'
   });
 
-  return jsonp(url, data, options);
+  return axios.get(url, {
+    params: data
+  }).then((res) => {
+    return Promise.resolve(res.data);
+  })
 }
