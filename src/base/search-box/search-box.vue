626c56b910a53e2005f964ef87390babf74bb952
AliceYuling <yyl1215@126.com>
2017-11-19 21:18:40 +0800

    add search box

---
+++ src/base/search-box/search-box.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="search-box">
+    <i class="icon-search"></i>
+    <input class="box" v-model="query" :placeholder="placeholder">
+    <i v-show="query" @click="clear" class="icon-dismiss"></i>
+  </div>
+</template>
+
+<script type="ecmascript-6">
+  export default {
+    props: {
+      placeholder: {
+        type: String,
+        default: 'search for song'
+      }
+    },
+    data () {
+      return {
+        query: ''
+      };
+    },
+    methods: {
+      clear () {
+        this.query = '';
+      }
+    },
+    created () {
+      this.$watch('query', (newQuery) => {
+        this.$emit('query', newQuery);
+      });
+    }
+  };
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import '~common/style/variable.styl'
+  .search-box
+    display: flex
+    align-items: center 
+    box-sizing: border-box
+    width: 100%
+    height: 40px
+    padding: 0 6px
+    border-radius: 6px
+    background: $color-highlight-background
+    .icon-search:
+      font-size: $font-size-large-x
+      color: $color-background
+    .box
+      display: block
+      flex: 1
+      line-height: 24px
+      background: $color-highlight-background
+    .icon-dismiss
+      font-size: $font-size-medium
+      color: $color-background
+</style>
--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,14 +1,28 @@
 <template>
   <div class="search">
-    search
+    <div class="search-box-wrapper">
+      <search-box></search-box>
+    </div>
   </div>
 </template>
 
-<script type="text/ecmascript-6">
-  export default {};
+<script type="cmascript-6">
+  import SearchBox from 'base/search-box/search-box.vue';
+  export default {
+    components: {
+      SearchBox
+    }
+  };
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
+  @import '~common/style/variable.styl'
+  @import '~common/style/mixin.styl'
   .search
-    color: #fff
+    .search-box-wrapper
+      margin: 20px
+    .shortcut-wrapper
+      position: fixed
+      top: 178px
+      bottom: 0
 </style>
